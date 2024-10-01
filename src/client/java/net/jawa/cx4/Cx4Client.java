c130871d9f9e8ed6b4da5b805f57a46dd0cb439f
DrewB-5 <fivedmb@gmail.com>
2024-10-01 00:48:36 -0400

    Initial commit

---
+++ src/client/java/net/jawa/cx4/Cx4Client.java
@@ -0,0 +1,10 @@
+package net.jawa.cx4;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Cx4Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/jawa/cx4/Cx4.java
@@ -0,0 +1,16 @@
+package net.jawa.cx4;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Cx4 implements ModInitializer {
+	public static final String MOD_ID = "cx4";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
